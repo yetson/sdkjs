e3989fc46dce6bbb1cc94dd1d4bec627a8e77606
GoshaZotov <Igor.Zotov@onlyoffice.com>
2016-03-30 14:12:00 +0300

    add function asc_getTableRange(class asc_CFormatTableInfo)

--- cell/view/WorksheetView.js
+++ cell/view/WorksheetView.js
@@ -7302,6 +7302,7 @@
 			}
 			cell_info.formatTableInfo.lastRow = curTablePart.TotalsRowCount !== null ? true : false;
 			cell_info.formatTableInfo.firstRow = curTablePart.HeaderRowCount === null ? true : false;
+			cell_info.formatTableInfo.tableRange = curTablePart.Ref.getAbsName();
 			
 			//cell_info.formatTableInfo.filterButton = curTablePart.HeaderRowCount !== null ? true : false;
 		}
