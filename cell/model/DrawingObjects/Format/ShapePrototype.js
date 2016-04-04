1f3c83f7cb62c85b1d1405ce139d0ff7b0bbc993
Alexey.Musinov <alexey.musinov@onlyoffice.com>
2016-04-04 12:19:53 +0300

    [mobile]  AscDisableTextSelection - флаг запрета перехода в режим редактирования

--- cell/model/DrawingObjects/Format/ShapePrototype.js
+++ cell/model/DrawingObjects/Format/ShapePrototype.js
@@ -123,6 +123,10 @@ CShape.prototype.hitInTextRect = function (x, y)
     }
     else
     {
+        if (window.AscDisableTextSelection)
+            return;
+
+
         return this.hitInTextRectWord(x, y);
     }
 
