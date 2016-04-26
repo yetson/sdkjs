27271a455d7646b30a9d45725fe3e815ccd8c2d0
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2016-04-26 12:38:33 +0300

    slide/themes/Themes to function-closure

--- slide/apiExport.js
+++ slide/apiExport.js
@@ -22,9 +22,7 @@
  * Pursuant to Section 7  3(e) we decline to grant you any rights under trademark law for use of our trademarks.
  *
 */
-if (undefined === window['Asc']) {
-  window['Asc'] = {};
-}
+window['Asc'] = window['Asc'] || {};
 window['Asc']['asc_docs_api'] = asc_docs_api;
 asc_docs_api.prototype['asc_GetFontThumbnailsPath'] = asc_docs_api.prototype.asc_GetFontThumbnailsPath;
 asc_docs_api.prototype['pre_Save'] = asc_docs_api.prototype.pre_Save;
@@ -491,10 +489,6 @@ CAscSlideProps.prototype['get_LockTranzition'] = CAscSlideProps.prototype.get_Lo
 CAscSlideProps.prototype['put_LockTranzition'] = CAscSlideProps.prototype.put_LockTranzition;
 CAscSlideProps.prototype['get_LockRemove'] = CAscSlideProps.prototype.get_LockRemove;
 CAscSlideProps.prototype['put_LockRemove'] = CAscSlideProps.prototype.put_LockRemove;
-CAscThemeInfo.prototype['get_Name'] = CAscThemeInfo.prototype.get_Name;
-CAscThemeInfo.prototype['get_Url'] = CAscThemeInfo.prototype.get_Url;
-CAscThemeInfo.prototype['get_Image'] = CAscThemeInfo.prototype.get_Image;
-CAscThemeInfo.prototype['get_Index'] = CAscThemeInfo.prototype.get_Index;
 CAscThemes.prototype['get_EditorThemes'] = CAscThemes.prototype.get_EditorThemes;
 CAscThemes.prototype['get_DocumentThemes'] = CAscThemes.prototype.get_DocumentThemes;
 window['Asc']['CAscChartProp'] = CAscChartProp;
