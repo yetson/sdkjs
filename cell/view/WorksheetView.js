1172aafbe5ffda0cfa76068aeec325a43aeb83c3
GoshaZotov <Igor.Zotov@onlyoffice.com>
2016-04-27 17:45:04 +0300

    Bug #32334 - [Copy&Paste] Бесконечно висит ромашка при вставке форматированной таблицы в полностью объединенную строку

--- cell/view/WorksheetView.js
+++ cell/view/WorksheetView.js
@@ -9227,6 +9227,9 @@
             bIsUpdate = false;
             AscCommonExcel.buildRecalc( t.model.workbook );
             AscCommonExcel.unLockDraw( t.model.workbook );
+			if ( callTrigger ) {
+                t.handlers.trigger( "slowOperation", false );
+            }
             return;
         }
         this.expandColsOnScroll(false, true);
