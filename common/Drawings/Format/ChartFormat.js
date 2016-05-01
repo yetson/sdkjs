df83380675ca42762bee59529fb2428992c60e30
SergeyLuzyanin <Sergey.Luzyanin@onlyoffice.com>
2016-05-01 23:28:19 +0300

    Возможность выставлять заголовки диаграмм

--- common/Drawings/Format/ChartFormat.js
+++ common/Drawings/Format/ChartFormat.js
@@ -2019,7 +2019,7 @@ CPlotArea.prototype =
                 return axis_by_types.valAx[i];
         }
 
-        for(var i = 0; i < axis_by_types.valAx.length; ++i)
+        for(var i = 0; i < axis_by_types.catAx.length; ++i)
         {
             if(axis_by_types.catAx[i].axPos === AX_POS_B || axis_by_types.catAx[i].axPos === AX_POS_T)
                 return axis_by_types.catAx[i];
@@ -2039,7 +2039,7 @@ CPlotArea.prototype =
                 return axis_by_types.valAx[i];
         }
 
-        for(var i = 0; i < axis_by_types.valAx.length; ++i)
+        for(var i = 0; i < axis_by_types.catAx.length; ++i)
         {
             if(axis_by_types.catAx[i].axPos === AX_POS_L || axis_by_types.catAx[i].axPos === AX_POS_R)
                 return axis_by_types.catAx[i];
