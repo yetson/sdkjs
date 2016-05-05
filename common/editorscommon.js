02ac55bcd545d137e0987ab8e43c4ce9db9aa517
Alexander.Trofimov <Alexander.Trofimov@onlyoffice.com>
2016-05-05 14:02:18 +0300

    relative path to sdk-all

--- common/editorscommon.js
+++ common/editorscommon.js
@@ -2665,7 +2665,7 @@ CUserCacheColor.prototype.init = function(nColor) {
     document.head.appendChild(script);
   }
   function loadSdk(sdkName, callback) {
-    loadScript('../../../../sdkjs/' + sdkName + '/sdk-all.js', callback);
+    loadScript('/sdkjs/' + sdkName + '/sdk-all.js', callback);
   }
 
 var g_oIdCounter = new CIdCounter();
