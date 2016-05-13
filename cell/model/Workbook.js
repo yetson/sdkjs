a2eaaee683b3cca8547c26073a70758c955df916
konovalovsergey <Sergey.Konovalov@onlyoffice.com>
2016-05-13 14:27:37 +0300

    conditionalFormattingXfs

--- cell/model/Workbook.js
+++ cell/model/Workbook.js
@@ -5472,7 +5472,7 @@ Cell.prototype.compileXfs=function(){
 			if(null != this.compiledXfs)
 				this.compiledXfs = this.conditionalFormattingXfs.merge(this.compiledXfs);
 			else
-				this.compiledXfs = this.xfs;
+				this.compiledXfs = this.conditionalFormattingXfs;
 		}
 	}
 };
